f79373633f63021039f61bcc5158a443036e6eb5
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-03-13 10:37:15 +0530

    Modified inst menu code

--- Assets/Scripts/Main_Menu/InstructionsMenu.cs
+++ Assets/Scripts/Main_Menu/InstructionsMenu.cs
@@ -4,34 +4,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class InstructionsMenu : MonoBehaviour
-{
-    [SerializeField] private GameObject _palette1, _palette2, _introVideo, _nextButton;
-    [SerializeField] private GameObject _palette3, _palette4, _splEnemyVideo, _playButton, _backButton;
+{   
+    [SerializeField] private List<GameObject> gameObjects = new List<GameObject>();
 
     public void NextButtonClicked()
     {
-        _introVideo.SetActive(false);
-        _palette1.SetActive(false);
-        _palette2.SetActive(false);
-        _nextButton.SetActive(false);
-        _splEnemyVideo.SetActive(true);
-        _palette3.SetActive(true);
-        _palette4.SetActive(true);
-        _playButton.SetActive(true);
-        _backButton.SetActive(true);
+        for(int i = 0; i < gameObjects.Count; i++)
+        {
+            if(i < 4)
+                gameObjects[i].SetActive(false);
+            else
+                gameObjects[i].SetActive(true);
+        }
     }
 
     public void BackButtonClicked()
     {
-        _introVideo.SetActive(true);
-        _palette1.SetActive(true);
-        _palette2.SetActive(true);
-        _nextButton.SetActive(true);
-        _splEnemyVideo.SetActive(false);
-        _palette3.SetActive(false);
-        _palette4.SetActive(false);
-        _playButton.SetActive(false);
-        _backButton.SetActive(false);
+        for (int i = 0; i < gameObjects.Count; i++)
+        {
+            if (i < 4)
+                gameObjects[i].SetActive(true);
+            else
+                gameObjects[i].SetActive(false);
+        }
     }
 
     public void PlayButtonClicked()
